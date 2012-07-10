c74bdc9043d685bb89e6ffe8ab32967f2b18dc08
bidaliu <bidaliu@sohu-inc.com>
2012-07-10 10:23:36 +0800

    1. 去掉restfulframework

--- SohukanHealth/SohukanHealth/settings.py
+++ SohukanHealth/SohukanHealth/settings.py
@@ -130,7 +130,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
     'monitor',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
