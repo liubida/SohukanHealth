0b3d73ce0594bde436a330f0282a4b178d4b169b
sohukandev <sohukandev@sohu-inc.com>
2013-03-25 10:59:32 +0800

    fix exportsql

--- SohukanHealth/exportSQL.py
+++ SohukanHealth/exportSQL.py
@@ -15,15 +15,14 @@ import anyjson
 FILE_SIZE = 10000
 
 def process(date):
-    index = 0
+    index = 57 
     if not os.path.exists('mysql'):
         os.mkdir('mysql')
-    time = 2
     try:
         conn = MySQLdb.connect(**c.db_self_config)
         cursor = conn.cursor()
-        while(time > 0):
-            sql = "select * from stats_operobject where gmt_create < %s limit %s" % (date.strftime('%Y-%m-%d'), FILE_SIZE)
+        while(True):
+            sql = "select * from stats_operobject where gmt_create < '%s' limit %s" % (date.strftime('%Y-%m-%d'), FILE_SIZE)
             cursor.execute(sql)
             results = cursor.fetchall()
             if len(results) == 0:
@@ -31,7 +30,7 @@ def process(date):
             index += 1
             content = ''
             for r in results:
-                dic = {"id": "%s" % r[0], "user_id": "%s" % r[1], "oper_id": "%s" % r[2], "object_type": r[3], "object_key": r[4], "gmt_create": r[5].strftime("%Y-%m-%d"), "gmt_modify": r[6].strftime("%Y-%m-%d")}
+                dic = {"id": "%s" % r[0], "user_id": "%s" % r[1], "oper_id": "%s" % r[2], "object_type": r[3], "object_key": r[4], "gmt_create": r[5].strftime("%Y-%m-%d %H:%M:%S"), "gmt_modify": r[6].strftime("%Y-%m-%d %H:%M:%S")}
                 content += anyjson.dumps(dic) + '\n'
             w = open('mysql/operobject_%s_%s.txt' % (datetime.datetime.strftime(date, '%Y-%m-%d'), index), 'w')
             w.writelines(content)
@@ -41,7 +40,6 @@ def process(date):
             conn.commit()
             if len(results) < FILE_SIZE:
                 break
-            time -= 1
     except Exception, e:
         print e
     finally:
